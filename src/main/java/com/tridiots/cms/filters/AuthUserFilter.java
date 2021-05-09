f1a2a2c5b888c3d4dd59a022a0ef4dc9e6e8d103
Elroy Kanye <elroykanye@gmail.com>
2021-05-09 15:55:18 +0100

    arranged checks for all user roles

--- src/main/java/com/tridiots/cms/filters/AuthUserFilter.java
+++ src/main/java/com/tridiots/cms/filters/AuthUserFilter.java
@@ -1,36 +1,102 @@
 package com.tridiots.cms.filters;
 
-import com.tridiots.cms.models.User;
-import com.tridiots.cms.shortcuts.IO;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.tridiots.cms.models.User;
+import com.tridiots.cms.shortcuts.IO;
+
 import java.io.IOException;
 
-public class AuthUserFilter extends AuthFilter{
+public class AuthUserFilter implements Filter {
+
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+    	HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+		HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+		
+		int adminRoleCode = 111;
+		if(roleSessionChecker(httpRequest, adminRoleCode)) {
+			IO.println("Session valid");
+            filterChain.doFilter(httpRequest, httpResponse);
+		} else {
+			IO.println("Session invalid");
+            httpRequest.getSession().setAttribute("errorMessage", "You have to be logged in first!");
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
+		}
+        
 
-        HttpSession session = httpRequest.getSession();
-        if(session != null) {
-            User loggedInUser = (User) session.getAttribute("loggedInUser");
-
-            if(loggedInUser != null) {
-                IO.println("Session valid");
-                filterChain.doFilter(httpRequest, httpResponse);
-                //httpResponse.sendRedirect(httpRequest.getContextPath() + "/user/profile.jsp");
-            } else {
-                IO.println("Session invalid");
-                httpRequest.getSession().setAttribute("errorMessage", "You have to be logged in first!");
-                httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.jsp");
-            }
-        }
     }
+    
+    public class AuthAdminFilter implements Filter {
+
+		@Override
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+				throws IOException, ServletException {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+    		HttpServletResponse httpResponse = (HttpServletResponse) response;
+    		
+    		int adminRoleCode = 111;
+    		if(roleSessionChecker(httpRequest, adminRoleCode)) {
+    			IO.println("Admin session valid");
+    			chain.doFilter(httpRequest, httpResponse); 
+    		} else {
+    			IO.println("Admin session invalid");
+    			httpResponse.sendRedirect(httpRequest.getContextPath() + "/error/403.html");
+    		}
+			
+		}
+    	
+    }
+    
+    public class AuthJudgeFilter implements Filter {
+    	@Override
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+				throws IOException, ServletException {
+    		HttpServletRequest httpRequest = (HttpServletRequest) request;
+    		HttpServletResponse httpResponse = (HttpServletResponse) response;
+    		
+    		int judgeRoleCode = 222;
+    		if(roleSessionChecker(httpRequest, judgeRoleCode)) {
+    			IO.println("Judge session valid");
+    			chain.doFilter(httpRequest, httpResponse); 
+    		} else {
+    			IO.println("Judge session invalid");
+    			httpResponse.sendRedirect(httpRequest.getContextPath() + "/error/403.html");
+    		}
+		}
+    }
+    
+    public class AuthContestantFilter implements Filter {
+    	@Override
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+				throws IOException, ServletException {
+    		HttpServletRequest httpRequest = (HttpServletRequest) request;
+    		HttpServletResponse httpResponse = (HttpServletResponse) response;
+    		
+    		int contestantRoleCode = 333;
+    		if(roleSessionChecker(httpRequest, contestantRoleCode)) {
+    			IO.println("COntestant session valid");
+    			chain.doFilter(httpRequest, httpResponse);
+    		} else {
+    			IO.println("Contestant session invalid");
+    			httpResponse.sendRedirect(httpRequest.getContextPath() + "/error/403.html");
+    		}
+			
+		}
+    }
+    
+    public boolean roleSessionChecker(HttpServletRequest httpRequest, int roleCode) {
+    	HttpSession session = httpRequest.getSession();
+    	
+    	if(session != null) {
+    		User loggedInUser = (User) session.getAttribute("loggedInUser");
+    		return (loggedInUser != null && loggedInUser.getUserRole() == roleCode);
+    	}
+    	return false;
+    }
+
 }
