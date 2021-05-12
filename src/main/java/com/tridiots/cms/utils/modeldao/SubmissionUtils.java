1a51abb6059dd2ff7f18546dd10d46206cadc3cc
Elroy Kanye <elroykanye@gmail.com>
2021-05-13 00:17:54 +0100

    model dao for Submission bean

---
+++ src/main/java/com/tridiots/cms/utils/modeldao/SubmissionUtils.java
@@ -0,0 +1,14 @@
+package com.tridiots.cms.utils.modeldao;
+
+import com.tridiots.cms.message.Message;
+import com.tridiots.cms.models.Submission;
+
+public class SubmissionUtils {
+	
+	public static Message addSubmission(Submission submission) {
+		
+		return null;
+		
+	}
+
+}
