577d1fae768e73c320935598c99866bbf891e9d2
Elroy Kanye <elroykanye@gmail.com>
2021-05-15 11:25:46 +0100

    removed unused cj import

--- src/main/java/com/tridiots/cms/utils/modeldao/SubmissionUtils.java
+++ src/main/java/com/tridiots/cms/utils/modeldao/SubmissionUtils.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import com.mysql.cj.jdbc.result.ResultSetImpl;
 import com.tridiots.cms.message.Message;
 import com.tridiots.cms.models.Submission;
 import com.tridiots.cms.utils.dbutils.ConnectionUtils;
