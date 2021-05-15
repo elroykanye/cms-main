f11e899891d58d9e694f2a3246c5a528f9d3bf0a
Elroy Kanye <elroykanye@gmail.com>
2021-05-15 11:15:18 +0100

    attached grade updating to subId

--- src/main/java/com/tridiots/cms/utils/modeldao/SubmissionUtils.java
+++ src/main/java/com/tridiots/cms/utils/modeldao/SubmissionUtils.java
@@ -7,12 +7,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.mysql.cj.jdbc.result.ResultSetImpl;
 import com.tridiots.cms.message.Message;
 import com.tridiots.cms.models.Submission;
 import com.tridiots.cms.utils.dbutils.ConnectionUtils;
 import com.tridiots.cms.utils.dbutils.QueryUtils;
 
 public class SubmissionUtils {
+	private static final int JUDGE_NUMBER = 3;
 	private static PreparedStatement prepStatement = null;
 	private static Statement statement = null;
 	private static ResultSet resultSet = null;
@@ -93,6 +95,9 @@ public class SubmissionUtils {
 				submission.setSubmissionFinalGrade(resultSet.getDouble("submission_final_grade"));
 				submission.setContestantId(resultSet.getInt("contestant_id"));
 				
+				// updates the submission final grade while getting
+				updateSubmissionFinalGrade(submission.getSubmissionId());
+				
 				submissions.add(submission);
 			}
 		} catch(SQLException exception) {
@@ -102,6 +107,72 @@ public class SubmissionUtils {
 			ConnectionUtils.closeConnection(conn);
 		}
 		return submissions;
-		
+	}
+	
+	public static void updateAllSubmissions() {
+		ArrayList<Integer> allSubIds = new ArrayList<>();
+		String sql = "SELECT submission_id FROM wtaxy_submission";
+		try {
+			conn = ConnectionUtils.openConnection();
+			statement = conn.createStatement();
+			resultSet = statement.executeQuery(sql);
+			while(resultSet.next()) allSubIds.add(resultSet.getInt("submission_id"));
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		} finally {
+			QueryUtils.closeQueryObject(prepStatement, statement);
+			ConnectionUtils.closeConnection(conn);
+		} 
+		if(allSubIds.size() > 0) {
+			for(Integer subId : allSubIds) {
+				updateSubmissionFinalGrade(subId);
+			}
+		}
+	}
+	
+	public static void updateSubmissionFinalGrade(int subId) {
+		String sql = "UPDATE wtaxy_submission SET submission_final_grade=? WHERE submission_id=";
+		double updatedGrade = calcSubmissionFinalGrade(subId);
+		try {
+			conn = ConnectionUtils.openConnection();
+			prepStatement = conn.prepareStatement(sql);
+			prepStatement.setDouble(1, updatedGrade);
+			prepStatement.setInt(2, subId);
+			prepStatement.execute();
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		} finally {
+			QueryUtils.closeQueryObject(prepStatement);
+			ConnectionUtils.closeConnection(conn);
+		}
+	}
+	
+	private static double calcSubmissionFinalGrade(int subid) {
+		String query = "SELECT grade_value FROM wtaxy_grade WHERE submission_id=?";
+		ArrayList<Integer> gradeValues = new ArrayList<>();
+		try {
+			conn = ConnectionUtils.openConnection();
+			prepStatement = conn.prepareStatement(query);
+			prepStatement.setInt(1, subid);
+			resultSet = prepStatement.executeQuery();
+			while(resultSet.next()) {
+				gradeValues.add(resultSet.getInt("grade_value"));
+			}
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		} finally {
+			QueryUtils.closeQueryObjects(prepStatement, resultSet);
+			ConnectionUtils.closeConnection(conn);
+		}
+		return calcAverage(gradeValues);
+	}
+	
+	private static double calcAverage(ArrayList<Integer> values) {
+		if(values.size() == 0) return 0;
+		int sum = 0;
+		for(Integer val : values) {
+			sum += val;
+		}
+		return sum/JUDGE_NUMBER;
 	}
 }
